62e90b10e380007694ca63521b22152a7bbdbde9
Saksham Kashyap <sakshamshady@gmail.com>
2019-10-12 15:10:59 +0530

    Updates

---
+++ precache-manifest.2001cfdc3f2046ea50f59de125b64e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40795a51b1f528efc93e1b02c347fb2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "938c05618ec48246cece",
+    "url": "/my-app/static/css/main.102171d3.chunk.css"
+  },
+  {
+    "revision": "39236fcab3e371083e7c",
+    "url": "/my-app/static/js/2.e8fbbb73.chunk.js"
+  },
+  {
+    "revision": "938c05618ec48246cece",
+    "url": "/my-app/static/js/main.510afcf0.chunk.js"
+  },
+  {
+    "revision": "247eec36cf986955c323",
+    "url": "/my-app/static/js/runtime-main.fe96fb05.js"
+  }
+]);
